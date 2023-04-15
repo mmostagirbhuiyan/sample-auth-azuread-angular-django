7531e1cfa7acb9467cd322c87b2bc1e67c07d594
MBhuiyan <mmostagirbhuiyan@gmail.com>
2023-04-15 06:25:16 -0400

    Cert auto renewal

--- nginx/renew-certificate.sh
+++ nginx/renew-certificate.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-certbot renew --quiet && \
-cp /etc/letsencrypt/live/factsprovider.com/fullchain.pem /etc/ssl/certs/nginx.crt && \
-cp /etc/letsencrypt/live/factsprovider.com/privkey.pem /etc/ssl/private/nginx.key && \
+set -e
+
+# Renew SSL certificate
+certbot renew --quiet
+
+# Copy renewed SSL certificate files to Nginx container
+cp /etc/letsencrypt/live/factsprovider.com/fullchain.pem /etc/ssl/certs/nginx.crt
+cp /etc/letsencrypt/live/factsprovider.com/privkey.pem /etc/ssl/private/nginx.key
+
+# Reload Nginx
 nginx -s reload
