b90748187aca3d590f894c45039521128f4a6986
MBhuiyan <mmostagirbhuiyan@gmail.com>
2023-04-15 06:29:56 -0400

    Docker-compose update

--- nginx/renew-certificate.sh
+++ nginx/renew-certificate.sh
@@ -6,8 +6,8 @@ set -e
 certbot renew --quiet
 
 # Copy renewed SSL certificate files to Nginx container
-cp /etc/letsencrypt/live/yourdomain.com/fullchain.pem /etc/ssl/certs/nginx.crt
-cp /etc/letsencrypt/live/yourdomain.com/privkey.pem /etc/ssl/private/nginx.key
+cp /etc/letsencrypt/live/factsprovider.com/fullchain.pem /etc/ssl/certs/nginx.crt
+cp /etc/letsencrypt/live/factsprovider.com/privkey.pem /etc/ssl/private/nginx.key
 
 # Reload Nginx
 nginx -s reload
